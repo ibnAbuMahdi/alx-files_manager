f0c4472aa022012f3df5f05c4d7d072db0aa9ff6
Your Name <you@example.com>
2023-09-06 13:22:10 -0700

    resolved conflict with task_8

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 import { Router } from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
 import { getConnect, getDisconnect } from '../controllers/AuthController';
-<<<<<<< HEAD
 import { getShow, getIndex, postUpload } from '../controllers/FilesController';
 import { getMe, postNew } from '../controllers/UsersController';
-=======
-import { getShow, getIndex, getFile } from '../controllers/FilesController';
-import { getMe } from '../controllers/UserController';
->>>>>>> task_8
 
 const router = Router();
 
@@ -18,11 +13,7 @@ router.get('/disconnect', getDisconnect);
 router.get('/users/me', getMe);
 router.get('/files/:id', getShow);
 router.get('/files', getIndex);
-<<<<<<< HEAD
 router.post('/users', postNew);
 router.post('/files', postUpload);
-
-=======
 router.get('/files:id/data', getFile);
->>>>>>> task_8
 export default router;
